951c256d56e1bf7ba838e16dbe934543788d298b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-04-02 13:24:29 -0300

    agrego el docker pero sin que ejecute un comando en especial. todo lo de supervisor está para futuro. en el stack se le agrega a este contenedor las credenciales y se usa esa imagen para ejecutar el servicio. esta imagen es una imagen intermedia

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo docker build --no-cache --file docker/dockerfile -t google-model .
+#sudo docker build --no-cache --file docker/dockerfile -t google-model .
+docker build --file docker/dockerfile -t google-model .
